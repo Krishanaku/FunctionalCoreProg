558d0a8266b37274919b1b316e0ca14ed8ddfa3a
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-13 14:13:44 +0530

    Add : Program to Compute Quotient and Remainder

---
+++ QuotientandRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCoreProg
+{
+    public class QuotientandRemainder
+    {
+        public static void FindQuotientAndRemainder()
+        {
+            Console.WriteLine("Enter a Value to QuotientAndRemainder ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Dividend : {0} Divisor : {1}", divisor, quotient);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
