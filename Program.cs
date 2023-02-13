64f9adecb15bb4610a95899e79abe4cc7322efd5
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-13 12:55:04 +0530

    Add : FlipCoin Problem Solution

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FunctionalCoreProg
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Core Programming ");
+            FlipCoin.CalculatePercentage();
+            //LeapYear.LYearorNot();
+            //PrimeFactor.CheckPrime();
+            //Power2Number.CalPower2Num();
+            // Harmonic.Harmonicfind();
+            //QuotientandRemainder.FindQuotientAndRemainder();
+            //SwapTwoNumbers.SwapNumber();
+            //EvenorOdd.CheckEvenOdd();
+            //VowelorConsonant.CheckVAndCon();
+            //LargestAmong3Numbers.FindLargestNumber();
+
+        }
+    }
+}
+
