a24b08416fc58f1a4320651c30d6d9724d30565d
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-13 13:58:08 +0530

    Add : Harnomic Program

---
+++ Harmonic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCoreProg
+{
+    public class Harmonic
+    {
+        public static void Harmonicfind()
+        {
+            Console.WriteLine("Enter the a number for harmonic");
+            int N = Convert.ToInt32(Console.ReadLine());
+            double first = 0.0;
+
+            if (N > 0)
+            {
+
+                for (int i = 1; i < N; i++)
+                {
+                    first = first + (1.0 / i);
+                    Console.WriteLine("Harmonic : " + first);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter the value greater than 0 ");
+            }
+        }
+    }
+}
