52bf3009cdd3e981bd98fc733997019d68ef4dc7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-13 14:11:46 +0530

    Add : Primefactor Program

---
+++ PrimeFactor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCoreProg
+{
+    public  class PrimeFactor
+    {
+        public static void CheckPrime()
+        {
+            Console.WriteLine("Enter a value to check prime or not");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(N);
+
+            int i;
+            int a = 0;
+            for (i = 1; i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    a++;
+                }
+
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("Prime Number");
+                int fact = 1;
+                int factor;
+                for (i = 2; i <= N; i++)
+                {
+                    factor = fact * i; //fact = fact * i
+                    Console.Write(factor);
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not prime number");
+            }
+
+
+
+        }
+    }
+}
