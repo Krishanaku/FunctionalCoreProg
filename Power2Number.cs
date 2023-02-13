b8c9c3f175d32339465196fae5e3be9a6053a98c
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-13 13:47:07 +0530

    Add : Power of 2 number program

---
+++ Power2Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalCoreProg
+{
+    public  class Power2Number
+    {
+        public static void CalPower2Num()
+        {
+            int i;
+            int num = 2;
+            int N = 31;
+            for (i = 0; i <= N; i++)
+            {
+                double pow = Math.Pow(num, i);
+                Console.WriteLine("Calculated 2 power" + i + ":-" + pow);
+            }
+        }
+    }
+}
